8f6c2c5ed423ed289f6c21d00da18f9b51a49881
jniculescu <jani.niculescu@gmail.com>
2017-04-19 13:49:31 +0300

    Sidenav and toolbar added.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,9 @@ import {Component} from '@angular/core';
 
 export class AppComponent {
 
-  constructor() {
+  constructor(private router: Router) {
   }
-
+  navigateToLogin(){
+    this.router.navigate(['/login']);
+  }
 }
